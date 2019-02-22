a38d30c20fffe64d1f9608d3416584ebc574f871
Furtuna Virgil <virgil.furtuna@gmail.com>
2019-02-22 02:53:18 +0200

    Add the updated code

--- js/Board.js
+++ js/Board.js
@@ -1,11 +1,14 @@
-class Board
-{
+class Board {
     constructor() {
         this.rows = 6;
         this.columns = 7;
         this.spaces = this.createSpaces();
     }
-    // GENERATES 2D ARRAY OF SPACES
+
+    /** 
+     * Generates 2D array of spaces. 
+     * @return  {array}     An array of space objects
+     */
     createSpaces() {
         const spaces = [];
 
@@ -16,10 +19,16 @@ class Board
                 const space = new Space(x, y);
                 col.push(space);
             }
+
             spaces.push(col);
         }
+
         return spaces;
     }
+
+    /** 
+     * Draws associated SVG spaces for all game spaces.
+     */
     drawHTMLBoard() {
         for (let column of this.spaces) {
             for (let space of column) {
